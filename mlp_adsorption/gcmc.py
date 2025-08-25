Fix framework density calculation in GCMC and Widom classes

mlp_adsorption/gcmc.py
@@ -29,6 +29,7 @@ from mlp_adsorption.utilities import (
     random_position,
     random_rotation,
     vdw_overlap,
+    get_density
 )
 
 
@@ -120,7 +121,7 @@ class GCMC:
         self.set_framework(framework_atoms)
 
         # Get the framework density in g/cm^3
-        self.framework_density: float = self.get_framework_density()
+        self.framework_density: float = get_density(self.framework)
 
         # Adsorbate setup
         self.set_adsorbate(adsorbate_atoms)
@@ -238,16 +239,6 @@ class GCMC:
         self.adsorbate_energy = self.adsorbate.get_potential_energy()
         self.n_ads = len(self.adsorbate)
         self.adsorbate_mass = np.sum(self.adsorbate.get_masses()) / units.kg
-
-    def get_framework_density(self) -> float:
-        """
-        Get the density of the framework in g/cm^3
-        """
-
-        mass = np.sum(self.framework.get_masses()) / units.kg * 1e3    # Convert from amu to g
-        volume = self.V * (1e-8 ** 3)  # Convert from Angs^3 to cm^3
-
-        return mass / volume
 
     def set_state(self, state: ase.Atoms) -> None:
         """

mlp_adsorption/utilities.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ase import units
+from ase import units, Atoms
 from ase.cell import Cell
 from scipy.spatial.transform import Rotation
 
@@ -186,6 +186,16 @@ def vdw_overlap2(atoms, vdw, n_ads):
     return False
 
 
+def get_density(structure: Atoms) -> float:
+    """
+    Get the density of the framework in g/cm^3
+    """
+
+    mass = np.sum(structure.get_masses()) / units.kg * 1e3    # Convert from amu to g
+    volume = structure.get_volume() * (1e-8 ** 3)  # Convert from Angs^3 to cm^3
+
+    return mass / volume
+    
 def get_perpendicular_lengths(cell: Cell) -> tuple[float, float, float]:
     """
     Calculate the perpendicular lengths of a unit cell.

mlp_adsorption/widom.py
@@ -17,6 +17,7 @@ from mlp_adsorption.utilities import (
     get_perpendicular_lengths,
     random_position,
     vdw_overlap2,
+    get_density
 )
 
 
@@ -91,7 +92,7 @@ class Widom:
         self.framework_mass: float = np.sum(self.framework.get_masses()) / units.kg
 
         # Get the framework density in g/cm^3
-        self.framework_density: float = self.get_framework_density()
+        self.framework_density: float = get_density(self.framework)
 
         # Adsorbate setup
         self.adsorbate = adsorbate_atoms
@@ -116,16 +117,7 @@ class Widom:
         self.minimum_configuration: ase.Atoms = self.framework.copy()
         self.minimum_energy: float = 0
 
-    def get_framework_density(self) -> float:
-        """
-        Get the density of the framework in g/cm^3
-        """
 
-        mass = np.sum(self.framework.get_masses()) / units.kg * 1e3    # Convert from amu to g
-        volume = self.V * (1e-8 ** 3)  # Convert from Angs^3 to cm^3
-
-        return mass / volume
-    
     def print_header(self):
         """
         Print the header for the simulation output.